7bdd8ac3b8d1deafcdf1f75e218b065d61055e9d
Igor Kalinichenko <igor.kalinichenko@consultant.lego.com>
2024-07-14 13:37:13 +0300

    - updates scopes of the fixtures;

--- tests/subcutaneous/conftest.py
+++ tests/subcutaneous/conftest.py
@@ -9,22 +9,22 @@ from src.gym_management.application.common.interfaces.persistence.subscriptions_
 from src.gym_management.presentation.api.dependency_injection import DependencyContainer
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def di_container() -> containers.DeclarativeContainer:
     container = DependencyContainer()
     yield container
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def mediator(di_container) -> IMediator:
     return di_container.app.mediator.mediator()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def admins_repository(di_container) -> AdminsRepository:
     return di_container.infrastructure.admins_repository()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def subscriptions_repository(di_container) -> SubscriptionsRepository:
     return di_container.infrastructure.subscriptions_repository()
