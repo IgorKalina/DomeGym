8c0c0f6414d112ccc039ad623a94c3115657f578
Igor Kalinichenko <igor.kalinichenko@consultant.lego.com>
2024-07-14 15:29:00 +0300

    - adds unittests for domain events;

---
+++ tests/unit/gym_management/domain/gym/test_gym_domain_events.py
@@ -0,0 +1,38 @@
+from src.gym_management.domain.gym.events.room_added_event import RoomAddedEvent
+from tests.common.gym.gym_factory import GymFactory
+from tests.common.room.room_factory import RoomFactory
+from tests.common.subscription.subscription_factory import SubscriptionFactory
+
+
+class TestGymDomainEvents:
+    def test_gym_add_room_when_added_should_create_domain_event(self) -> None:
+        subscription = SubscriptionFactory.create_subscription()
+        gym = GymFactory.create_gym(max_rooms=subscription.max_rooms)
+        rooms = [RoomFactory.create_room() for _ in range(subscription.max_rooms)]
+
+        for room in rooms:
+            gym.add_room(room)
+
+        expected_domain_events = [
+            RoomAddedEvent(
+                room=room,
+                gym=gym,
+            )
+            for room in rooms
+        ]
+
+        assert gym.pop_domain_events() == expected_domain_events
+
+    def test_gym_add_rom_when_error_should_not_create_domain_event(self) -> None:
+        subscription = SubscriptionFactory.create_subscription()
+        gym = GymFactory.create_gym(max_rooms=subscription.max_rooms)
+        rooms = [RoomFactory.create_room() for _ in range(subscription.max_rooms)]
+
+        for room in rooms:
+            gym.add_room(room)
+        # flush domain events created for valid number of rooms
+        gym.pop_domain_events()
+
+        gym.add_room(RoomFactory.create_room())
+
+        assert subscription.pop_domain_events() == []
---
+++ tests/unit/gym_management/domain/subscription/test_subscription_domain_events.py
@@ -0,0 +1,35 @@
+from src.gym_management.domain.subscription.events.gym_added_event import GymAddedEvent
+from src.gym_management.domain.subscription.subscription_type import SubscriptionType
+from tests.common.gym.gym_factory import GymFactory
+from tests.common.subscription.subscription_factory import SubscriptionFactory
+
+
+class TestSubscriptionDomainEvents:
+    def test_subscription_add_gym_when_added_should_create_domain_event(self) -> None:
+        subscription = SubscriptionFactory.create_subscription(subscription_type=SubscriptionType.PRO)
+        gyms = [GymFactory.create_gym() for _ in range(subscription.max_gyms)]
+
+        for gym in gyms:
+            subscription.add_gym(gym)
+
+        expected_domain_events = [
+            GymAddedEvent(
+                subscription=subscription,
+                gym=gym,
+            )
+            for gym in gyms
+        ]
+
+        assert subscription.pop_domain_events() == expected_domain_events
+
+    def test_subscription_add_gym_when_error_should_not_create_domain_event(self) -> None:
+        subscription = SubscriptionFactory.create_subscription(subscription_type=SubscriptionType.PRO)
+        gyms = [GymFactory.create_gym() for _ in range(subscription.max_gyms)]
+        for gym in gyms:
+            subscription.add_gym(gym)
+        # flush domain events created for valid number of gyms
+        subscription.pop_domain_events()
+
+        subscription.add_gym(GymFactory.create_gym())
+
+        assert subscription.pop_domain_events() == []
