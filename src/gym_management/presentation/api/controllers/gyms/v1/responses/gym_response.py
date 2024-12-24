1b262d0e373054d4bb64056804e0661ecc1467c5
Igor Kalinichenko <igor.kalinichenko@consultant.LEGO.com>
2024-12-24 21:47:05 +0200

    - adds example;

--- src/gym_management/presentation/api/controllers/gyms/v1/responses/gym_response.py
+++ src/gym_management/presentation/api/controllers/gyms/v1/responses/gym_response.py
@@ -1,11 +1,13 @@
 import datetime
 import uuid
 
+from pydantic import Field
+
 from src.gym_management.presentation.api.controllers.common.responses.dto import ResponseData
 
 
 class GymResponse(ResponseData):
     id: uuid.UUID
     subscription_id: uuid.UUID
-    name: str
+    name: str = Field(examples=["MyGym"])
     created_at: datetime.datetime
