64dacdbf69010202ce8d0597642df83cb33dd258
Igor Kalinichenko <igor.kalinichenko@consultant.LEGO.com>
2024-12-26 20:37:31 +0200

    - adds default router;

--- src/gym_management/presentation/api/controllers/main.py
+++ src/gym_management/presentation/api/controllers/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from .default import default_router
 from .exceptions import setup_exception_handlers
 from .gyms.main import setup_gym_controllers
 from .subscriptions.main import setup_subscription_controllers
 
 
 def setup_controllers(app: FastAPI) -> None:
+    app.include_router(default_router)
     setup_gym_controllers(app)
     setup_subscription_controllers(app)
     setup_exception_handlers(app)
