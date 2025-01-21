0a205856e86dd22014d80ad872b3fe010f90b6af
Igor Kalinichenko <igor.kalinichenko@consultant.LEGO.com>
2025-01-21 10:26:10 +0200

    - adds rooms controllers;

--- src/gym_management/presentation/api/controllers/main.py
+++ src/gym_management/presentation/api/controllers/main.py
@@ -4,6 +4,7 @@ from .default import default_router
 from .exceptions import setup_exception_handlers
 from .gym.main import setup_gym_controllers
 from .healthcheck import healthcheck_router
+from .room.main import setup_room_controllers
 from .subscription.main import setup_subscription_controllers
 
 
@@ -12,4 +13,5 @@ def setup_controllers(app: FastAPI) -> None:
     app.include_router(healthcheck_router)
     setup_gym_controllers(app)
     setup_subscription_controllers(app)
+    setup_room_controllers(app)
     setup_exception_handlers(app)
