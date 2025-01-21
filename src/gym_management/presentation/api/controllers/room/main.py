1753cec387a5090afb3d8d819d2997d703e7af6c
Igor Kalinichenko <igor.kalinichenko@consultant.LEGO.com>
2025-01-21 10:26:48 +0200

    - updates name of the router;

--- src/gym_management/presentation/api/controllers/room/main.py
+++ src/gym_management/presentation/api/controllers/room/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from .v1.routes import router as gym_v1_router
+from .v1.routes import router as room_v1_router
 
 
 def setup_room_controllers(app: FastAPI) -> None:
-    app.include_router(gym_v1_router)
+    app.include_router(room_v1_router)
